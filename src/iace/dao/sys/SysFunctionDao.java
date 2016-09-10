482a82be242a25395a8095087d6f5236e35181e4
Eric Liao <eric855104@gmail.com>
2016-09-10 15:13:05 +0800

    系統功能列表改為用namespace排序

--- src/iace/dao/sys/SysFunctionDao.java
+++ src/iace/dao/sys/SysFunctionDao.java
@@ -1,6 +1,14 @@
 package iace.dao.sys;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
 import iace.dao.BaseIaceDao;
+import iace.entity.BaseEntity;
 import iace.entity.sys.SysFunction;
 
 public class SysFunctionDao extends BaseIaceDao<SysFunction> implements ISysFunctionDao {
@@ -9,4 +17,16 @@ public class SysFunctionDao extends BaseIaceDao<SysFunction> implements ISysFunc
 		super(SysFunction.class);
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<SysFunction> listAll() {
+		List<Criterion> criterionList = new ArrayList<Criterion>();
+		criterionList.add(Restrictions.eq("isValid", BaseEntity.TRUE));	
+		List<Order> orderList = new ArrayList<Order>();
+		orderList.add(Order.asc("namespace"));
+		orderList.add(Order.asc("id"));
+		return (List<SysFunction>) super.listAll(SysFunction.class, orderList, criterionList);
+	}
+
+	
 }
