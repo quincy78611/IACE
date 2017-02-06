1d0594a9e31a0e8f9fb1d3fc62df3bc7ec1080ca
Eric Liao <eric855104@gmail.com>
2017-02-06 10:28:34 +0800

    禁止複製訊息修正

--- WebContent/scripts/disableCopy.js
+++ WebContent/scripts/disableCopy.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$(".disableCopy").bind({
 		copy : function(e){
-			alert('禁止複製，請尊重撰稿人住著作權!');
+			alert('禁止複製，請尊重撰稿人著作權!');
 			e.preventDefault();
 			return false;
 		},
 		cut : function(){
-			alert('禁止複製，請尊重撰稿人住著作權!');
+			alert('禁止複製，請尊重撰稿人著作權!');
 			e.preventDefault();
 			return false;
 		}
