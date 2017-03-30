44888b87185d9064301c87caa56a9a725a337fe5
Eric Liao <eric855104@gmail.com>
2017-03-30 15:52:30 +0800

    新增練習用Action:媒合廣告

---
+++ src/practice/action/AdAction.java
@@ -0,0 +1,58 @@
+package practice.action;
+
+import iace.action.BaseIaceAction;
+
+public class AdAction extends BaseIaceAction {
+
+	private static final long serialVersionUID = 8189741101549201192L;
+
+	public AdAction() {
+		super();
+		super.setTitle("媒合廣告");
+	}
+
+	public String init() {
+		//TODO
+		return null;
+	}
+	
+	public String index() {
+		//TODO
+		return null;
+	}
+	
+	public String showDetail() {
+		//TODO
+		return null;
+	}
+	
+	public String create() {
+		//TODO
+		return null;
+	}
+	
+	public String createSubmit() {
+		//TODO
+		return null;
+	}
+	
+	public String update() {
+		//TODO
+		return null;
+	}
+	
+	public String updateSubmit() {
+		//TODO
+		return null;
+	}
+	
+	public String delete() {
+		//TODO
+		return null;
+	}
+	
+	public String deleteSubmit() {
+		//TODO
+		return null;
+	}
+}
